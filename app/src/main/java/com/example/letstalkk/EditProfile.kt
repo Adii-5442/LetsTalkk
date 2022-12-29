2eaf815f10f097ac9eee8b21b64b00429c787cdd
Aditya <adi.sh5442@gmail.com>
2022-12-29 10:13:52 +0530

    User uid generation

--- app/src/main/java/com/example/letstalkk/EditProfile.kt
+++ app/src/main/java/com/example/letstalkk/EditProfile.kt
@@ -3,11 +3,13 @@ package com.example.letstalkk
 import android.annotation.SuppressLint
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.letstalkk.databinding.ActivityEditProfileBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class EditProfile : AppCompatActivity() {
 
@@ -20,6 +22,12 @@ class EditProfile : AppCompatActivity() {
         setContentView(R.layout.activity_edit_profile)
         setSupportActionBar(findViewById(R.id.toolbar))
 
+        val user = FirebaseAuth.getInstance().currentUser
+        if(user!=null) {
+            Log.i("USER UID","${user.uid}")
+        }
+
+
         pictueToolBar = findViewById(R.id.toolbar_layout)
         editPicture = findViewById(R.id.editPic)
         editPicture.setOnClickListener { view ->
@@ -28,4 +36,5 @@ class EditProfile : AppCompatActivity() {
                 .setAction("Action", null).show()
         }
     }
+
 }
