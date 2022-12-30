6f7f8c2083c14386790f30311f69eb9859d022f0
Nailsonseat <aadarshv3@gmail.com>
2022-12-30 20:26:54 +0530

    Added image picker from camera or gallery. Permission remains to be fixed.

--- app/src/main/java/com/example/letstalkk/initialLoginProfileSetup.kt
+++ app/src/main/java/com/example/letstalkk/initialLoginProfileSetup.kt
@@ -8,13 +8,21 @@ import com.example.letstalkk.databinding.ActivityInitialLoginProfileSetupBinding
 import android.view.View
 import android.widget.*
 import android.app.DatePickerDialog
+import android.content.Intent
+import android.net.Uri
 import java.text.SimpleDateFormat
 import android.widget.DatePicker
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import droidninja.filepicker.FilePickerBuilder
+import droidninja.filepicker.FilePickerConst
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 import java.util.*
 
-class initialLoginProfileSetup : AppCompatActivity() {
+class initialLoginProfileSetup : AppCompatActivity(),EasyPermissions.PermissionCallbacks {
 
     private lateinit var binding: ActivityInitialLoginProfileSetupBinding
     private lateinit var fullName: EditText
@@ -24,7 +32,9 @@ class initialLoginProfileSetup : AppCompatActivity() {
     private lateinit var dateOfBirth : Button
     private lateinit var profilePic : FloatingActionButton
     private lateinit var finalButton : ExtendedFloatingActionButton
+    private lateinit var recyclerView: RecyclerView
     private var cal = Calendar.getInstance()
+    private var arraylist = ArrayList<Uri>();
 
     //Function to update button to display the chosen birth date by the user
     private fun updateDateInView() {
@@ -51,6 +61,7 @@ class initialLoginProfileSetup : AppCompatActivity() {
         genderSpinner = findViewById<Spinner>(R.id.genderSelect)
         profilePic=findViewById<FloatingActionButton>(R.id.editProfilePic)
         finalButton=findViewById<ExtendedFloatingActionButton>(R.id.finalizeProfile)
+        recyclerView=findViewById<RecyclerView>(R.id.recyclerView)
 
 
         //Pre defined Variables
@@ -101,5 +112,87 @@ class initialLoginProfileSetup : AppCompatActivity() {
 
         })
 
+
+        //Image Picker
+        profilePic.setOnClickListener({
+            if (EasyPermissions.hasPermissions(this,android.Manifest.permission.CAMERA) and EasyPermissions.hasPermissions(this,android.Manifest.permission.READ_EXTERNAL_STORAGE)){
+                imagePicker();
+            }
+            else{
+                EasyPermissions.requestPermissions(
+                    this,
+                    "Bhai permission dede camera ki",
+                    100,
+                    android.Manifest.permission.CAMERA
+                );
+                EasyPermissions.requestPermissions(
+                    this,
+                    "Bhai permission dede storage ki",
+                    100,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                );
+            }
+        })
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode,permissions,grantResults,this)
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        if (requestCode==100 && perms.size==2){
+
+
+            imagePicker()
+        }
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+
+        if(EasyPermissions.somePermissionPermanentlyDenied(this,perms)){
+
+
+            AppSettingsDialog.Builder(this).build().show()
+        }else{
+
+            Toast.makeText(applicationContext,"Permission Demied",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(resultCode== RESULT_OK&&data!=null){
+
+
+            if(requestCode==FilePickerConst.REQUEST_CODE_PHOTO){
+
+
+                arraylist= data.getParcelableArrayListExtra<Uri>(FilePickerConst.KEY_SELECTED_MEDIA)!!
+
+
+                recyclerView.layoutManager=LinearLayoutManager(this)
+
+                recyclerView.adapter= FilePickerClass(arraylist)
+            }
+        }
+    }
+
+
+
+    private fun imagePicker(){
+        FilePickerBuilder.instance
+            .setActivityTitle("Select Image")
+            .setSpan(FilePickerConst.SPAN_TYPE.FOLDER_SPAN,3)
+            .setSpan(FilePickerConst.SPAN_TYPE.DETAIL_SPAN,4)
+            .setMaxCount(1)
+            .setSelectedFiles(arraylist)
+            .setActivityTheme(R.style.FilePickerTheme)
+            .pickPhoto(this);
     }
 }
